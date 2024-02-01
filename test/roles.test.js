aee9992f33806cb1c2305b25c00ef6ff9888aca0
Volker Buzek <volker.buzek@js-soft.com>
2024-02-01 20:14:29 +0100

    test: roles

--- test/roles.test.js
+++ test/roles.test.js
@@ -1,27 +1,53 @@
 import { expect, describe, it, beforeEach, vitest } from "vitest"
 import auth from "../lib/auth"
-import { mockAaaIdToken, mockPrincipalToken } from "./__assets__/tokens"
+import { mockAaaIdToken } from "./__assets__/tokens"
 
 describe("auth", () => {
-  let mockRequst
+  let mockRequest
   let mockResponse
   let nextFn = vitest.fn()
 
   beforeEach(() => {
-    mockRequst = {}
+    mockRequest = {}
     mockResponse = {
       json: vitest.fn(),
     }
   })
-  it("should retrieve the correct roles", () => {
-    mockRequst = {
+  it("plug into cap/express flow", () => {
+    mockRequest = {
       headers: {
         "x-ms-client-principal-name": "test@outlook.com",
         "x-ms-token-aad-id-token": mockAaaIdToken,
       },
     }
-    auth(mockRequst, {}, nextFn)
+    auth(mockRequest, {}, nextFn)
 
     expect(nextFn).toHaveBeenCalledTimes(1)
+  })
+  it("should set req.user properly (incl roles)", () => {
+    mockRequest = {
+      headers: {
+        "x-ms-client-principal-name": "test@outlook.com",
+        "x-ms-token-aad-id-token": mockAaaIdToken,
+      },
+    }
+    auth(mockRequest, {}, nextFn)
+    expect(mockRequest.user.id).toBe("test@outlook.com")
+    expect(mockRequest.user.roles["authenticated-user"]).toBe(1)
+    expect(mockRequest.user.roles["37d959b3-1f20-4083-b312-6d216d6883bb"]).toBe(1)
+    
+  })
+  it("should handle roles correctly", () => {
+    mockRequest = {
+      headers: {
+        "x-ms-client-principal-name": "test@outlook.com",
+        "x-ms-token-aad-id-token": mockAaaIdToken,
+      },
+    }
+    auth(mockRequest, {}, nextFn)
+    const user = mockRequest.user
+    expect(user.is("authenticated-user")).toBe(true)
+    expect(user.is("any")).toBe(true)
+    expect(user.is("37d959b3-1f20-4083-b312-6d216d6883bb")).toBe(true)
   })
 })
